4c0a9da55c2f496b080c29f503ba7bb9bf831bcc
elly <eri.patent@gmail.com>
2020-06-22 23:59:41 +0900

    質問1,2に回答

--- task.rb
+++ task.rb
@@ -5,6 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names << "斎藤" 
+  puts names
 
 end
 
@@ -13,7 +15,8 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  array = array1 + array2
+  p array
 end
 
 def q3
