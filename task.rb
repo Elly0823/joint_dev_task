d8008558f69355b79b75461275154bc2935cb876
elly <eri.patent@gmail.com>
2020-07-03 22:51:46 +0900

    質問15の修正

--- task.rb
+++ task.rb
@@ -131,17 +131,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  data = [
-    { name: "saitou", hobby: "soccer", age: 33, role: "admin" },
-    { name: "yamada", hobby: "baseball", role: "normal" }
-  ]
-  data.each do |data|
-    if data.key?(:age) 
-      p "OK"
-    else
-      p "NG"
-    end
-  end
+  p data1.key?(:age) ? "OK" : "NG"
+  p data2.key?(:age) ? "OK" : "NG"
 end
 
 def q16
