107e0ed8781b898ef1ce7ba56fa1f9b67f7d953c
elly <eri.patent@gmail.com>
2020-07-14 16:13:37 +0900

    質問20、info_entry_feeの定義内で入場料金を変数に代入

--- task.rb
+++ task.rb
@@ -233,16 +233,17 @@ class Zoo
   end
 
   def info_entry_fee(user)
-    case user.age
+    admission = case user.age
     when 0..5
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:infant]}円です"
+      @entry_fee[:infant]
     when 6..12
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:children]}円です"
+      @entry_fee[:children]
     when 13..64
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:adult]}円です"
+      @entry_fee[:adult]
     when 65..120
-      puts "#{user.name}さんの入場料金は#{@entry_fee[:senior]}円です"
+      @entry_fee[:senior]
     end
+    puts "#{user.name}さんの入場料金は#{admission}円です"
   end
 
 end
@@ -250,6 +251,7 @@ end
 def q20
   # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
   zoo = Zoo.new(name: "旭山動物園", entry_fee: { infant: 0, children: 400, adult: 800, senior: 500 })
+
 
   users = [
     UserQ20.new(name: "たま", age: 3),
